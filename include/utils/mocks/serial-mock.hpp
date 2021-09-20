93e9085be787ec6dc020b3190e9ea396c0b1bc62
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2021-09-20 11:09:58 -0300

    Added features do the serial-mock's write and read methods.
    Serial's write function can write a string too. So, the serial mock
    version must provide such functionality. Also, in order to be able
    to mock serial behavior while looking for patterns on serial on read,
    the serial-mock's read function was modified to provide such a
    functionality.

--- include/utils/mocks/serial-mock.hpp
+++ include/utils/mocks/serial-mock.hpp
@@ -1,17 +1,16 @@
 // Copyright 2021 Time Energy Industria e Comercio Ltda
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 
 #pragma once
 
@@ -32,6 +31,14 @@ public:
 
     void write(const std::vector<uint8_t> &buffer) override;
 
+    void write(std::string msg) override;
+
     size_t read(std::vector<uint8_t> &buffer, size_t nBytesToRead, std::chrono::nanoseconds timeout) override;
+
+    size_t read(std::vector<uint8_t> &buffer, std::vector<uint8_t> &pattern, size_t nBytesToRead,
+                std::chrono::nanoseconds timeout) override;
+
+    size_t read(std::vector<uint8_t> &buffer, std::vector<uint8_t> &&pattern, size_t nBytesToRead,
+                std::chrono::nanoseconds timeout) override;
 };
 }    // namespace mock
--- src/utils/mocks/serial-mock.cpp
+++ src/utils/mocks/serial-mock.cpp
@@ -1,18 +1,18 @@
 // Copyright 2021 Time Energy Industria e Comercio Ltda
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
+#include <algorithm>
 #include "serial-mock.hpp"
 
 using namespace std;
@@ -26,17 +26,44 @@ void Serial::write(const vector<uint8_t> &buffer)
     request.insert(request.end(), buffer.begin(), buffer.end());
 }
 
+void Serial::write(string msg)
+{
+    Serial::write(vector<uint8_t>(msg.cbegin(), msg.cend()));
+}
+
 size_t Serial::read(vector<uint8_t> &buffer, size_t nBytesToRead, nanoseconds timeout)
 {
-    nBytesToRead = nBytesToRead < response.size() ? nBytesToRead : response.size();
+    std::vector<uint8_t> empty;
+    return read(buffer, empty, nBytesToRead, timeout);
+}
 
-    buffer.reserve(buffer.size() + nBytesToRead);
+size_t Serial::read(std::vector<uint8_t> &buffer, std::vector<uint8_t> &pattern, size_t nBytesToRead,
+                    std::chrono::nanoseconds timeout)
+{
+    bool patternFound = false;
 
-    copy(begin(response), begin(response) + nBytesToRead, back_inserter(buffer));
+    size_t totalRead = nBytesToRead < response.size() ? nBytesToRead : response.size();
 
-    response.erase(begin(response), begin(response) + nBytesToRead);
+    if (!pattern.empty()) {
+        auto res = search(response.begin(), response.begin() + totalRead, pattern.begin(), pattern.end());
+        if (res != response.begin() + totalRead) {
+            totalRead = distance(begin(response), res + pattern.size());
+            patternFound = true;
+        }
+    }
 
-    return nBytesToRead;
+    buffer.reserve(buffer.size() + totalRead);
+
+    copy(begin(response), begin(response) + totalRead, back_inserter(buffer));
+
+    response.erase(begin(response), begin(response) + totalRead);
+
+    return !pattern.empty() ? (patternFound ? totalRead : -totalRead) : totalRead;
+}
+
+size_t Serial::read(vector<uint8_t> &buffer, vector<uint8_t> &&pattern, size_t nBytesToRead, nanoseconds timeout)
+{
+    return read(buffer, pattern, nBytesToRead, timeout);
 }
 
 void Serial::setResponse(const vector<uint8_t> &response)
